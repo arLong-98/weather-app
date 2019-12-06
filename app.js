5d40ad163c0764f9f08701d6a72b5a8209b6e196
arLong-98 <dhairya999pateriya@gmail.com>
2019-12-07 01:36:46 +0530

    bug removed, enter a location to get weather details

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 
-forecast(-75.7088,44.1545,(error,data)=>{
-        console.log('error: '+error);
-        console.log('data: '+JSON.stringify(data));
-})
 
 
 
-geocode('Georgia',(error,data)=>{
+
+geocode('Madhya Pradesh',(error,data)=>{
         console.log('error: '+error);
         console.log('Data: '+JSON.stringify(data));
+
+        forecast(data.lattitude,data.longitude,(error,data)=>{
+                console.log('error: '+error);
+                console.log('data: '+JSON.stringify(data));
+        });
 });
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const geocode = (address,callback)=>{
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?types=country&access_token=pk.eyJ1IjoiZGhhaXJ5YTIzIiwiYSI6ImNrM3U2bjFwZTA5bHkza29wOTl2MTJyd2cifQ.ytyKZHCmpmBDZ6f2JKi3lg&limit=1';
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiZGhhaXJ5YTIzIiwiYSI6ImNrM3U2bjFwZTA5bHkza29wOTl2MTJyd2cifQ.ytyKZHCmpmBDZ6f2JKi3lg&limit=1';
 
     request({url:url,json:true}, (error,response)=>{
             if(error)
