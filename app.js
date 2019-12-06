0ccd5da49745c8af089878d3404c23213143b9d3
arLong-98 <dhairya999pateriya@gmail.com>
2019-12-07 02:00:42 +0530

    functionality added, enter a location from CLI and get weather update in console

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast')
+const forecast = require('./utils/forecast');
 
 
+const address = process.argv[2];
+//get location from command line
+if(!address)
+{
+        console.log('Enter an address !');
+}
 
+else 
+{
+        geocode(address,(error,data)=>{
 
+                if(error)
+                        return console.log('error: '+ error);
 
-geocode('Madhya Pradesh',(error,data)=>{
-        console.log('error: '+error);
-        console.log('Data: '+JSON.stringify(data));
-
-        forecast(data.lattitude,data.longitude,(error,data)=>{
-                console.log('error: '+error);
-                console.log('data: '+JSON.stringify(data));
-        });
-});
+                forecast(data.lattitude,data.longitude,(error,forecastdata)=>{
+                        if(error)
+                                return console.log('error: '+error);
+                        console.log(data.location);
+                        console.log(forecastdata);
+                });
+        });     
+}
