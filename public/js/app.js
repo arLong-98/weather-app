5c13e6c50b07f417f98610323a0d0540a803b246
arLong-98 <dhairya999pateriya@gmail.com>
2019-12-09 00:03:34 +0530

    functionality improved. *renders html to show weather on web page on submit.

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,27 @@
-console.log('Client side js file loaded');
 
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//         response.json().then((data)=>{
-//             console.log(data);
-//         });
-// });
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+
+const msgOne = document.querySelector('#msgOne');
+const msgTwo = document.querySelector('#msgTwo');
 
 weatherForm.addEventListener('submit',(event)=>{
     event.preventDefault(); //prevents default action which is to refresh the page
 
     const location = search.value;
     const url = 'http://localhost:3000/weather?address='+location;
+
+    msgOne.textContent = 'Loading...';
+
     fetch(url).then((response)=>{
         response.json().then((data)=>{
             if(data.error)
-                return console.log(data.error);
-            
-                console.log(data.location);
-                console.log(data.forecastdata);
-        })
-    })
+                msgOne.textContent = error;
+            else{
+                msgOne.textContent = data.location;
+                msgTwo.textContent = data.forecastdata;
+            }
+        });
+    });
 });
